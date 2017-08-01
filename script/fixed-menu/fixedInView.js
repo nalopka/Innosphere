071dc84bdddffd7c206b83fef8bf50b31a4261cc
Nalopka <Nalopka@gmail.com>
2017-08-01 16:53:22 +0700

    important fixed-menu fix

--- script/fixed-menu/fixedInView.js
+++ script/fixed-menu/fixedInView.js
@@ -1,23 +1,25 @@
+// SHOW MENU
 $(document).ready(function() {
-	inView.offset(50);
-	inView("header").on("exit", function() {
+	 $(window).scroll(function() {
+	var scrollTop = $(document).scrollTop();
+	if (scrollTop > 500) {
 		$(".fixed-menu").css("opacity", "1");
-	});
-	inView("header").on("enter", function() {
+	} else { 
 		$(".fixed-menu").css("opacity", "0");
+	}
 	});
 });
+
+// HIGHLIGHT ELEMENTS
 $(document).ready(function() {
 
 	inView.threshold(0.7);
 	inView('.section').on('enter', function(el){
-	console.log($(el));
 	var blockName = $(el).attr('id');
 	$('.fixed-menu').find('a[href="#'+blockName+'"]').addClass("active-fixed");
 	});
 
 	inView('.section').on('exit', function(el){
-	console.log($(el));
 	var blockName = $(el).attr('id');
 	$('.fixed-menu').find('a[href="#'+blockName+'"]').removeClass("active-fixed");
 	});
